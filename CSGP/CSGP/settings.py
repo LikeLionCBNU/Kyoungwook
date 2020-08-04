c29a3c2e94699a8c7849acc3f759098b17b4ad26
Ho <ii8858@naver.com>
2020-08-04 16:03:30 +0900

    naver, apple

--- CSGP/CSGP/settings.py
+++ CSGP/CSGP/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'b_board',
+    'stock',
 ]
 
 MIDDLEWARE = [
@@ -103,15 +106,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
---
+++ CSGP/stock/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import requests
+import json
+import xmltodict
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html', {})
+
+def stock(request):
+	api_requests = requests.get('https://cloud.iexapis.com/stable/stock/aapl/book?token=pk_83abfdf02247408c9e54f6370e95cfa9')
+	api_requests = requests.get('https://cloud.iexapis.com/stable/stock/aapl/book?token=pk_83abfdf02247408c9e54f6370e95cfa9')
+	krx_api_requests = requests.get('http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=035420').content.strip()
+
+	try :
+		api = json.loads(api_requests.content)
+		krx_api_to_str =  json.dumps(xmltodict.parse(krx_api_requests))
+		krx_api_to_str = krx_api_to_str.replace('@','')
+		krx_api = json.loads(krx_api_to_str)
+
+	except Exception as e:
+		api = "error"
+
+	return render(request, 'stock.html', {'stock_info' : api['quote'], 'krx_info' : krx_api['stockprice']['TBL_StockInfo'] })
